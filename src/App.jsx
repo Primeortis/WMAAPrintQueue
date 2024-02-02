0ff71263a54759324431413851226a383db822b4
BrysonV10 <brysonvanryn@outlook.com>
2024-02-02 18:32:02 -0500

    add signout button functionality in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import { useState } from 'react'
 import Navbar from '../components/navbar/nav';
 import styles from "./pagestyles.module.css"
 import { Button } from '@mui/material';
+import { useNavigate } from "react-router-dom"
+
 
 function App() {
+  let navigate = useNavigate();
+  function signOut(){
+    navigate("/auth");
+  }
+
+
   return (
     <>
       <Navbar admin={true}/>
@@ -13,7 +21,7 @@ function App() {
           <p>Email: email@email.com</p>
           <p>User ID: sldkfjwoeiurjsdjflwlskdjflsdfkj (Click to Copy)</p>
           <p>Status: Administrator</p>
-          <Button variant="contained">Sign Out</Button>
+          <Button variant="contained" onClick={signOut}>Sign Out</Button>
         </div>
       </div>
     </>
