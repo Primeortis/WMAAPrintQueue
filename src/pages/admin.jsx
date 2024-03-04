d41b402363d1b13a0f53c595d8850a9295090cac
J.P. King <dajeepk@gmail.com>
2024-03-04 09:50:58 -0500

    Added admin page

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -3,8 +3,14 @@ import Navbar from "../../components/navbar/nav";
 import styles from "../pagestyles.module.css"
 import {firebaseApp} from "../../src/firebase-config.js"
 import {getAuth, signOut} from "firebase/auth"
+import { useNavigate } from "react-router-dom";
 
 export default function AdminPage(props){
+    let navigate = useNavigate();
+    const auth = getAuth(firebaseApp);
+
+    let userInformation = auth.currentUser;
+
     return (
         <>
             <Navbar admin={true}/>
