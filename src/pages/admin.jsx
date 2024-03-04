20a9c8ccbb1fc119a070e3e445624ee172177872
J.P. King <dajeepk@gmail.com>
2024-03-04 10:20:12 -0500

    Admin page properly built, missing functions to go to certain pages to complete actions

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -14,7 +14,16 @@ export default function AdminPage(props){
     return (
         <>
             <Navbar admin={true}/>
-            
+            <div className={styles.body} style={{paddingTop:"5vh"}}>
+                <h1>Admin Control Panel</h1>
+                <div className={styles.popout}>
+                    <p>Choose your adventure</p>
+                    <Button variant="contained" onClick="Control Users" style={{margin:".5em"}}>Edit Users</Button>
+                    <Button variant="contained" onClick="Edit Queue" style={{margin:".5em"}}>Rearrange Queue</Button>
+                    <Button variant="contained" onClick="View All Files" style={{margin:".5em"}}>View All Files</Button>
+                    <Button variant="contained" onClick="" style={{margin:".5em"}}>View all prints</Button>
+                </div>
+            </div>
         </>
     )
 }
