23fb1c26ac80761c06e893ddf099b171444300cf
J.P. King <dajeepk@gmail.com>
2024-03-06 10:02:06 -0500

    View All Files Admin page functioning

--- src/pages/admin/viewAllFiles.jsx
+++ src/pages/admin/viewAllFiles.jsx
@@ -1,28 +1,53 @@
-import { Button } from "@mui/material";
-import Navbar from "../../../components/navbar/nav.jsx";
-import styles from "../../pagestyles.module.css";
+import { Button, LinearProgress } from "@mui/material";
+import Navbar from "../../../components/navbar/nav";
+import styles from "../../pagestyles.module.css"
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import {firebaseApp} from "../../../src/firebase-config.js"
 import {getAuth} from "firebase/auth"
-import { useNavigate } from "react-router-dom";
-import {getFirestore, doc, setDoc} from "firebase/firestore";
+import { getFirestore, collection, getDocs, query } from "firebase/firestore";
 
+function File(props){
+    return (
+      <Link className={[styles.rows, styles.dimOnHover].join(" ")} style={{cursor:"pointer", color:"black"}} to={"/file/"+props.id}>
+          <p className={styles.emP}>{props.name}</p>
+          <p>
+              <i>{props.date}</i>
+          </p>
+      </Link>
+    )
+}
 
 export default function FileViewerPage(props){
-    let navigate = useNavigate();
-    const auth = getAuth(firebaseApp);
-
-    let userInformation = auth.currentUser;
-
+    let [files, setFiles] = useState([]);
+    useEffect(()=> {
+      const db = getFirestore(firebaseApp);
+      const auth = getAuth(firebaseApp);
+      const q = query(collection(db, "files"));
+      async function getUserFiles(){
+        var querySnapshot = await getDocs(q);
+        console.log(querySnapshot);
+        let docs = [];
+        querySnapshot.forEach((doc)=> {
+          let data = doc.data();
+          docs.push(<File id={doc.id} name={data.name} date={data.date} key={doc.id}/>);
+        })
+        setFiles(docs);
+      }
+      if(files.length==0) getUserFiles();
+    }, [])
     return (
         <>
-            <Navbar admin={true}/>
-            <div className={styles.body} style={{paddingTop:"5vh"}}>
-                <h1>Manage User</h1>
-                <div className={styles.popout}>
-                    <p>Input user to edit permissions</p>
-                    
-                </div>
+          <Navbar admin={true}/>
+          <div className={styles.body} style={{paddingTop:"10vh"}}>
+            <h1>All Files</h1>
+            <div className={styles.popout} style={{textAlign:"left"}}>
+              {files.length>0?files:<LinearProgress />}
+              <br/>
+              <Button variant={"contained"} sx={{display:"block", margin:"auto", width: "fit-content"}} component={Link} to={"/file/new"}>Create New File</Button>
+              <br/>
             </div>
+          </div>
         </>
-    )
+      );
 }
