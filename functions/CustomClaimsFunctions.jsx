0eb71dba0931af24ba8398c5c53b62179bfa702f
J.P. King <dajeepk@gmail.com>
2024-03-08 10:13:36 -0500

    Added files to git ignore

--- functions/CustomClaimsFunctions.jsx
+++ functions/CustomClaimsFunctions.jsx
@@ -1,8 +1,17 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+const {logger} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+
+// The Firebase Admin SDK to access Firestore.
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+
 import {setCustomUserClaims} from "firebase-tools"
 import {firebaseApp} from "../src/firebase-config.js"
 import {getAuth} from "firebase/auth"
 
-function setLevel(levelInput){
+/*function setLevel(levelInput){
     const auth = getAuth(firebaseApp);
     auth.setCustomUserClaims(auth.currentUser.uid, {level: levelInput});
 };
@@ -15,4 +24,4 @@ function checkAdmin(){
     return false;
 };
 
-export {setLevel, checkAdmin};
+export {setLevel, checkAdmin};*/
