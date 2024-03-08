c44b543de2dc95b856066c684e31eac3d9ff308c
BrysonV10 <brysonvanryn@outlook.com>
2024-03-08 10:14:45 -0500

    Add error handling for paused user and update paused page layout

--- src/pages/paused.jsx
+++ src/pages/paused.jsx
@@ -1,11 +1,17 @@
 import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import Parent from "../../components/basic/parent";
 
 export default function PausedPage(props){
     return (
-        <div style={{width:"100vw", overflow:"none", top:"0"}}>
-            <img src="/wmaa.png" style={{width:"10vw", height:"auto", margin:"auto"}}/>
-            <h1>Your access to the WMAA Print Portal has been paused.</h1>
-            <PauseCircleIcon/>
-        </div>
+        <Parent>
+            <h1 style={{color:"black"}}>Paused</h1>
+            <div style={{backgroundColor:"#FAFAFA", width:"40%", margin:"auto", borderRadius:"10px", paddingBottom:"10px"}}>
+                <br/>
+                <img src="/wmaa.png"/><br/>
+                <PauseCircleIcon style={{width:"20vh", height:"auto", color:"black"}}/>
+                <p style={{color:"black", fontSize:"1em"}}>Your access to the WMAA Print Portal has been paused</p>
+                <p style={{color:"black"}}><i>Questions? Contact your system administrator</i></p>
+            </div>
+        </Parent>
     )
 }
