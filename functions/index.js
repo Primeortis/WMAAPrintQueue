e439b7d3c2855d01d4f59f667c4a76a8fad846ea
BrysonV10 <brysonvanryn@outlook.com>
2024-03-19 14:47:46 -0400

    added a working sample cloud function...

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+const {logger} = require("firebase-functions");
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const {initializeApp} = require("firebase-admin/app");
+initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.addmessage = onRequest(async (req, res)=> {
+    res.send("Hello from Cloud Function!");
+})
