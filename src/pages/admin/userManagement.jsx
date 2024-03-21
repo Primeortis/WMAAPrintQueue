8ad3ccb2c5f41b481a297a8d857caac75408c1c4
primeortis <dajeepk@gmail.com>
2024-03-21 14:56:02 -0400

    Finally, Bryson isn't an admin!

--- src/pages/admin/userManagement.jsx
+++ src/pages/admin/userManagement.jsx
@@ -1,4 +1,4 @@
-import { Button, LinearProgress, TextField } from "@mui/material";
+import { Box, Button, LinearProgress, Select, MenuItem, Modal, TextField } from "@mui/material";
 import Navbar from "../../../components/navbar/nav.jsx";
 import styles from "../../pagestyles.module.css";
 import {firebaseApp} from "../../../src/firebase-config.js"
@@ -9,14 +9,16 @@ import { useState } from "react";
 import ConfirmModal from "../../../components/confirmModal.jsx";
 
 export default function UserManagementPage(props){
+    const auth = getAuth(firebaseApp);
+    const functions = getFunctions(firebaseApp);
     let [userInformation, setUserInformation] = useState({});
     let [userIDOrEmail, setUserIDOrEmail] = useState("");
     let [loadingInformation, setLoadingInformation] = useState(false);
-    let [modalOpen, setModalOpen] = useState(false);
-    let [modalMessage, setModalMessage] = useState("");
+    let [modal1Open, setModal1Open] = useState(false);
+    let [modal2Open, setModal2Open] = useState(false);
+    let [modal2Message, setModal2Message] = useState("");
+    let [userLevel, setUserLevel] = useState(null);
     let navigate = useNavigate();
-    const auth = getAuth(firebaseApp);
-    const functions = getFunctions(firebaseApp);
 
     // REMOVE BELOW IN PRODUCTION
     connectFunctionsEmulator(functions, "localhost", 5001);
@@ -48,9 +50,24 @@ export default function UserManagementPage(props){
         }
     }
 
+    function editUserButton(){
+        setModal1Open(true)
+    }
+
     function pauseUserButton(){
-        setModalMessage("Are you sure you want to " + (userInformation.disabled?"unpause the user?":"pause the user?"))
-        setModalOpen(true)
+        setModal2Message("Are you sure you want to " + (userInformation.disabled?"unpause the user?":"pause the user?"))
+        setModal2Open(true)
+    }
+
+    function applyUserEdits(){
+        let updateUser = httpsCallable(functions, "setrole");
+        updateUser({uid: userInformation.uid, role: userLevel}).then((result)=>{
+            console.log(result)
+            setTimeout(()=>getUserInformation(), 500);
+        }).catch((error)=>{
+            console.error(error)
+        })
+        setModal1Open(false);
     }
 
     function pauseUser(){
@@ -61,6 +78,7 @@ export default function UserManagementPage(props){
         }).catch((error)=>{
             console.error(error)
         })
+        setModal2Open(false);
     }
 
     return (
@@ -81,9 +99,25 @@ export default function UserManagementPage(props){
                         <p>Email: {userInformation.email}</p>
                         <p>Role: {userInformation.role}</p>
                         <p>Last Signed In At: {userInformation.lastSignIn}</p>
-                        <Button variant="contained">Edit User</Button>
+                        <Button variant="contained" onClick={editUserButton}>Edit User</Button>
+                        {modal1Open?
+                            <Modal open={modal1Open} onClose={()=>{setModal1Open(false)}}>
+                                <Box sx={{width: "80%", backgroundColor:"rgba(91,91,91,0.8)", margin:"auto", padding:"2px", marginTop:"5vh"}}>
+                                    <h1>Edit Info</h1>
+                                    <p>Set Role</p>
+                                    <Select variant="filled" style={{width: "25%", minWidth:"40px"}} value = {userLevel} onChange = {(e)=>{setUserLevel(e.target.value)}}>
+                                        <MenuItem value="apprentice">Set to Apprentice</MenuItem>
+                                        <MenuItem value="journeyman">Set to Journeyman</MenuItem>
+                                        <MenuItem value="master">Set to Master</MenuItem>
+                                        <MenuItem value="admin">Set as Admin</MenuItem>
+                                    </Select>
+                                    <p></p>
+                                    <Button variant="contained" onClick={applyUserEdits}>Save and Leave</Button>
+                                </Box>
+                            </Modal>
+                        :null}
                         <Button variant="contained" onClick={pauseUserButton}>{userInformation.disabled?"Unpause User":"Pause User"}</Button>
-                        {modalOpen?<ConfirmModal message={modalMessage} onConfirm={pauseUser} onCancel={()=>{setModalOpen(false)}}/>:null}
+                        {modal2Open?<ConfirmModal message={modal2Message} onConfirm={pauseUser} onCancel={()=>{setModal2Open(false)}}/>:null}
                         </>
                     :<p>Enter a search query above...</p>}
                 </div>
