0e7907c40310712851b046ea75b6fe84c3e16fc1
BrysonV10 <brysonvanryn@outlook.com>
2024-03-30 09:47:21 -0400

    added printer management tabover plus some features

--- src/pages/classroom.jsx
+++ src/pages/classroom.jsx
@@ -1,15 +1,21 @@
 import {useState, useEffect} from 'react';
 import styles from '../pagestyles.module.css';
 import {getAuth} from 'firebase/auth';
-import { getFirestore, collection, getDocs, doc } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, doc, setDoc } from 'firebase/firestore';
 import { firebaseApp } from '../firebase-config';
-import { Button, IconButton, MenuItem, Select } from '@mui/material';
+import { Button, IconButton, MenuItem, Select,Modal, Box, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import SchoolIcon from '@mui/icons-material/School';
 import PersonIcon from '@mui/icons-material/Person';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import {getStorage, ref, getDownloadURL} from 'firebase/storage';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import CheckIcon from '@mui/icons-material/Check';
+import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
+import BuildIcon from '@mui/icons-material/Build';
+
 
 
 function QueueRow(props){
@@ -63,23 +69,90 @@ function QueueRow(props){
 }
 
 
+function StatusIcon(props){
+    if(props.status == "printing"){
+        return <SettingsIcon sx={{verticalAlign:"middle", marginRight:"5px", color: "#D90404"}}/>
+    } else if(props.status == "wait"){
+        return <PriorityHighIcon sx={{verticalAlign:"middle", marginRight:"5px", color: "#D90404"}}/>
+    } else if(props.status == "good"){
+        return <CheckIcon sx={{verticalAlign:"middle", marginRight:"5px", color: "#068701"}}/>
+    } else if(props.status == "no service"){
+        return <DoNotDisturbIcon sx={{verticalAlign:"middle", marginRight:"5px", color: "#D90404"}}/>
+    }
+}
+
+
+function PrinterRow(props){
+    let msg = "";
+    if(props.statusCode == "printing"){
+        msg = "Currently Printing"
+    } else if(props.statusCode == "wait"){
+        msg = "Awaiting Print Removal Confirmation"
+    } else if(props.statusCode == "good"){
+        msg = "Ready to Print"
+    } else if(props.statusCode == "no service"){
+        msg = "Out of Service"
+    }
+    return (
+        <div className={styles.rows}>
+            <p className={styles.emP}>{props.printer}</p>
+            <p onClick={() => props.onStatusUpdate({statusCode: props.statusCode, name: props.printer})} style={{cursor: "pointer"}}>
+                <StatusIcon status={props.statusCode}/>
+                <i>{msg}</i>
+            </p>
+            <IconButton onClick={()=> {props.onButtonClick(props.printer)}}>
+                <BuildIcon/>
+            </IconButton>
+        </div>
+    )
+}
+
+
 const ClassroomPage = () => {
     let goto = encodeURIComponent(window.location.pathname);
     let [categories, setCategories] = useState([]);
     let [selectedCategory, setSelectedCategory] = useState("");
     let [selectedMenu, setSelectedMenu] = useState("queue");
     let [queueItems, setQueueItems] = useState([]);
+    let [printers, setPrinters] = useState([]);
+    let [maintenanceModalOpen, setMaintenanceModalOpen] = useState("false");
+    let [maintenanceLogs, setMaintenanceLogs] = useState([]);
+    let [newMaintenanceMsg, setNewMaintenanceMsg] = useState("");
+    let [newPrinterStateModalOpen, setNewPrinterStateModalOpen] = useState(false);
+    let [newPrinterState, setNewPrinterState] = useState("");
+    let [pendingPrinterTime, setPendingPrinterTime] = useState("0:00");
+    let [pendingPrinterTimeMsg, setPendingPrinterTimeMsg] = useState("");
+    let [printerBeingEdited, setPrinterBeingEdited] = useState("");
+
     const auth = getAuth(firebaseApp);
     const navigate = useNavigate();
+    const db = getFirestore(firebaseApp);
+    const ref = collection(db, "categories")
+
+    async function getPrinterStatuses(){
+        console.log("getting printers")
+        const q = collection(db, "printers");
+        let querySnapshot = await getDocs(q);
+        let docs = [];
+        querySnapshot.forEach((doc)=> {
+            let data = doc.data();
+            docs.push(<PrinterRow printer={doc.id} statusCode={data.status} key={doc.id} onStatusUpdate={(state)=>{setPrinterBeingEdited(state.name);setNewPrinterState(state.statusCode);setNewPrinterStateModalOpen(true);}} onButtonClick={getMaintenanceLogs}/>)
+        })
+        setPrinters(docs);
+    }
+
     useEffect(()=>{
+        // checking authentication status
         if(!auth.currentUser) navigate("/auth?rd="+ goto);
         auth.currentUser.getIdTokenResult().then((idTokenResult) => {
             if(idTokenResult.claims.role == "admin"){let admin = true;}
           }).catch((err)=> {
             console.error(err)
           })
-        const db = getFirestore(firebaseApp);
-        const ref = collection(db, "categories")
+        
+        
+
+        // retrieving categories
         async function getCategories(){
             let querySnapshot = await getDocs(ref);
             let docs = [];
@@ -90,7 +163,33 @@ const ClassroomPage = () => {
             setCategories(docs);
         }
         getCategories();
+
+        // getting printer statuses
+        
+        if(printers.length==0) getPrinterStatuses();
     }, [])
+
+    function getMaintenanceLogs(printer){
+        const db = getFirestore(firebaseApp);
+        const ref = doc(db, "printers", printer);
+        const q = collection(ref, "maintenance");
+        async function getDocuments(){
+            let querySnapshot = await getDocs(q);
+            let docs = [];
+            querySnapshot.forEach((doc)=> {
+                let data = doc.data();
+                docs.push(<p key={data.date}>{data.user} {data.date}: {data.message}</p>)
+            })
+            console.log(docs)
+            if(docs.length ==0){
+                docs.push(<p key="none">No Maintenance Logs</p>)
+            }
+            setMaintenanceLogs(docs)
+            setMaintenanceModalOpen(printer);
+        }
+        getDocuments();
+    }
+
 
     function viewQueueButton(){
         
@@ -120,6 +219,64 @@ const ClassroomPage = () => {
         viewQueue()
     }
 
+    function submitNewMaintenanceMessage(){
+        const db = getFirestore(firebaseApp);
+        const ref = doc(db, "printers", maintenanceModalOpen);
+        const q = collection(ref, "maintenance");
+        let currentUser = getAuth().currentUser.email;
+        currentUser = currentUser.split("@")[0];
+        async function addDocument(user){
+            await addDoc(q, {
+                user: user,
+                date: new Date().toISOString(),
+                message: newMaintenanceMsg
+            })
+        }
+        addDocument(currentUser);
+        setMaintenanceLogs([...maintenanceLogs, <p key={new Date().toISOString()}>{currentUser} {new Date()}: {newMaintenanceMsg}</p>])
+        setNewMaintenanceMsg("");
+        setMaintenanceModalOpen(false);
+    }
+
+    function setTimeField(input){
+        let regex = new RegExp("^\\d{2}:\\d{2}$");
+        if(regex.test(input)==false){
+            setPendingPrinterTimeMsg("Invalid Time Format - Follow the format hh:mm");
+        } else if(!input.includes(":")){
+            setPendingPrinterTimeMsg("Invalid Time Format - Include a colon between hours and minutes, even if hours is zero");
+        } else {
+            setPendingPrinterTimeMsg("");
+        }
+        
+        
+        setPendingPrinterTime(input);
+    }
+
+    function updatePrinterState(){
+        const db = getFirestore(firebaseApp);
+        const ref = doc(db, "printers", printerBeingEdited);
+        async function updateState(){
+            let regex = new RegExp("^\\d{2}:\\d{2}$");
+            if(!regex.test(pendingPrinterTime)){
+                return;
+            }
+            if(newPrinterState == "printing"){
+                let time = pendingPrinterTime.split(":");
+                let hours = parseInt(time[0]);
+                let minutes = parseInt(time[1]);
+                let currentTime = new Date();
+                currentTime.setHours(currentTime.getHours() + hours);
+                currentTime.setMinutes(currentTime.getMinutes() + minutes);
+                await setDoc(ref, {status: newPrinterState, timeToDone: currentTime.toISOString()}, {merge: true});
+            } else {
+                await setDoc(ref, {status: newPrinterState, timeToDone: "none"}, {merge: true});
+            }
+        }
+        updateState();
+        setNewPrinterStateModalOpen(false);
+        getPrinterStatuses();
+    }
+
     return (
         <>
         <div className={styles.body} style={{paddingTop:"5vh"}}>
@@ -141,10 +298,53 @@ const ClassroomPage = () => {
                 
                 {queueItems}
                 </>:
+
+
                 <>
-                
+                {printers}
                 </>
                 }
+
+
+                    
+                    <Modal open={maintenanceModalOpen!="false"} onClose={()=>{setMaintenanceModalOpen("false")}}>
+                        <Box sx={{width: "80%", backgroundColor:"rgba(219,219,219,0.8)", margin:"auto", padding:"2px", marginTop:"5vh", color:"black"}}>
+                            <h1>Maintenance Logs</h1>
+                            {maintenanceLogs}
+                            <TextField label="Add Maintenance Log Message" sx={{width: "90%"}} value={newMaintenanceMsg} onChange={(e)=>setNewMaintenanceMsg(e.target.value)}/>
+                            <Button onClick={submitNewMaintenanceMessage}>Submit</Button>
+                        </Box>
+                    </Modal>
+                    
+
+                    
+                    <Modal open={newPrinterStateModalOpen} onClose={()=>{setNewPrinterStateModalOpen(false)}}>
+                        <Box sx={{width: "80%", backgroundColor:"rgba(219,219,219,0.8)", margin:"auto", padding:"2px", marginTop:"5vh", color:"black"}}>
+                            <h1>Set New Printer State</h1>
+                            <p>{printerBeingEdited}</p>
+                            <Select value={newPrinterState} onChange={(e)=>setNewPrinterState(e.target.value)}>
+                                <MenuItem value="printing">Printing</MenuItem>
+                                <MenuItem value="wait">Awaiting Print Removal</MenuItem>
+                                <MenuItem value="good">Ready to Print</MenuItem>
+                                <MenuItem value="no service">Out of Service</MenuItem>
+                            </Select>
+                            {newPrinterState=="printing"?
+                            <>
+                            <br/>
+                            <br/>
+                            <TextField label="Time to Print (hh:mm)" value={pendingPrinterTime} onChange={(e)=>setTimeField(e.target.value)}/>
+                            {pendingPrinterTimeMsg}
+                            </>
+                            :
+                            null
+                            }
+                            <br/>
+                            <Button variant="contained" onClick={updatePrinterState}>Submit</Button>
+                        </Box>
+                    </Modal>
+                    
+
+
             </div>
         </div>
         </>
